c0780c3456948553eb22a4457e160db3512dc4af
Gau9811 <gauravbajaj837@gmail.com>
2024-03-14 22:28:50 +0530

    new changes in root

--- .cache/root.js
+++ .cache/root.js
@@ -46,6 +46,11 @@ class LocationHandler extends React.Component {
        element.innerHTML = ""
        element.innerHTML = ".css-zxi5dp-Footer{display:none}"
     }
+
+    if(  element.innerHTML.search(".css-92jnlk") !== -1 !== -1){
+      element.innerHTML = ""
+      element.innerHTML = ".css-92jnlk{display:none}"
+   }
     
   }
     const slicesContext = {

b80c1847141371dbd618251f4ab7e88fe3677498
Gau9811 <gauravbajaj837@gmail.com>
2024-03-14 18:35:09 +0530

    initialize branch

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\gaura\\OneDrive\\Desktop\\portfoliowebsite\\.cache\\dev-404-page.js")),
+  "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": preferDefault(require("C:\\Users\\gaura\\OneDrive\\Desktop\\portfoliowebsite\\node_modules\\@lekoarts\\gatsby-theme-cara\\src\\templates\\cara.tsx")),
+  "component---src-pages-404-tsx": preferDefault(require("C:\\Users\\gaura\\OneDrive\\Desktop\\portfoliowebsite\\src\\pages\\404.tsx"))
+}
+
